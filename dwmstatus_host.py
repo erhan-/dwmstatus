d6dae26eec08da2540ffda69cff89cdea8907eb1
erhan <erhan@development.sicher.lab>
2020-05-20 19:09:39 +0200

    Initial commit

---
+++ dwmstatus_host.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import subprocess
+import re
+import socket
+from datetime import datetime
+from time import sleep
+
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+acpi_regex = r"([\w]+), ([\d]{1,2})%, ([\d:]*)"
+
+while True:
+    acpi_output = subprocess.check_output(['acpi']).decode("utf-8") 
+    acpi_match = re.search(acpi_regex, acpi_output)
+    if acpi_match:
+        mode = acpi_match.group(1)
+        battery_percentage = acpi_match.group(2)
+        time_remaining = acpi_match.group(3)
+        status_icon = '+' if mode == 'Charging' else '-'
+        now = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        hostname = socket.gethostname()
+        ip_address = get_ip_address()
+        status_string = f"{hostname}({ip_address}) | {now} | [{status_icon}] {battery_percentage}% ({time_remaining})"
+        subprocess.call(['xsetroot', '-name', status_string])
+    sleep(1)
---
+++ dwmstatus_vm.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import subprocess
+import re
+import socket
+from datetime import datetime
+from time import sleep
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+while True:
+    now = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    hostname = socket.gethostname()
+    ip_address = get_ip_address()
+    status_string = f"VM: {hostname}({ip_address}) | {now}"
+    subprocess.call(['xsetroot', '-name', status_string])
+    subprocess.call(['xrandr', '--output', 'Virtual-0', '--auto'])
+    sleep(2)
